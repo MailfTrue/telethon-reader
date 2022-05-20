430bffba146dac2683f54e758cfc6ff22dbdcf1e
mailf <mailf.dev@yandex.ru>
2022-05-20 16:28:50 +0300

    add env vars support

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import os
 import argparse
 
 from telethon import TelegramClient, utils, events
 from loguru import logger
+
+
+class EnvDefault(argparse.Action):
+    def __init__(self, env_var, required=True, default=None, **kwargs):
+        if not default and env_var:
+            if env_var in os.environ:
+                default = os.environ[env_var]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required,
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
 
 
 def get_client(session_path: str, api_id: int, api_hash: str, proxy: dict = None):
@@ -24,16 +39,18 @@ def get_client(session_path: str, api_id: int, api_hash: str, proxy: dict = None
 
 def main():
     parser = argparse.ArgumentParser(description='Telegram reader')
-    parser.add_argument('phone', help="Telegram phone", type=int)
-    parser.add_argument('session-path', help="Path to .session")
-    parser.add_argument('api-id', help="App api_id from https://my.telegram.org/apps", type=int)
-    parser.add_argument('api-hash', help="App api_hash from https://my.telegram.org/apps")
+    parser.add_argument('phone', help="Telegram phone", action=EnvDefault, env_var="PHONE", type=int)
+    parser.add_argument('session-path', action=EnvDefault, env_var="SESSION_PATH", help="Path to .session")
+    parser.add_argument('api-id', action=EnvDefault, env_var="API_ID",
+                        help="App api_id from https://my.telegram.org/apps", type=int)
+    parser.add_argument('api-hash', action=EnvDefault, env_var="API_HASH",
+                        help="App api_hash from https://my.telegram.org/apps")
 
-    parser.add_argument('--proxy-host')
-    parser.add_argument('--proxy-port')
-    parser.add_argument('--proxy-username')
-    parser.add_argument('--proxy-password')
-    parser.add_argument('--proxy-type')
+    parser.add_argument('--proxy-host', action=EnvDefault, env_var="PROXY_HOST", required=False)
+    parser.add_argument('--proxy-port', action=EnvDefault, env_var="PROXY_PORT", required=False)
+    parser.add_argument('--proxy-username', action=EnvDefault, env_var="PROXY_USERNAME", required=False)
+    parser.add_argument('--proxy-password', action=EnvDefault, env_var="PROXY_PASSWORD", required=False)
+    parser.add_argument('--proxy-type', action=EnvDefault, env_var="PROXY_TYPE", required=False)
 
     args = vars(parser.parse_args())
 
